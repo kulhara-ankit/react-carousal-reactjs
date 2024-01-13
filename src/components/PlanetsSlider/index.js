aa004459cbdb6853708db8959a85f37fdaf9a14d
kulhara-ankit <a.kulhara7@gmail.com>
2024-01-13 18:43:33 +0000

    React Carousal

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,22 @@
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    <div data-testid="planets" className="planets-app-container">
+      <h1 className="heading">PLANETS</h1>
+      <Slider>
+        {planetsList.map(each => (
+          <PlanetItem key={each.id} planetDetails={each} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
